39fc3aaa9923afe4a347fe81f4ec1a34330045ad
pate9011 <pate9011@student.acitech.org>
2024-11-20 10:31:14 -0500

    11.20.24
    -Finished the overwriting of a file and creating a new file
    -have to add the functionatlity of decoding a file.

--- enigma_template.py
+++ enigma_template.py
@@ -11,12 +11,12 @@ alphabet = "abcdefghijklmnopqrstuvwxyz"
 
 # user inputs a message and selects a key (or random), the message is then translated using the cipher
 def encode_message(dinput):
-        rotation_input = input("Enter a rotation cipher key (Press Enter for a random value): ")
+        basic_input = input("Enter a rotation cipher key (Press Enter for a random value): ")
 
-        if rotation_input.strip() == "":
+        if basic_input.strip() == "":
             rotation = random.randint(1, 25)  # Random value between 1 and 25
         else:
-            rotation = int(rotation_input) % 26  # Ensure rotation is within 0-25
+            rotation = int(basic_input) % 26  # Ensure rotation is within 0-25
 
         encoded_message = ""
 
@@ -27,13 +27,24 @@ def encode_message(dinput):
                 encoded_message += shifted_char
             else:
                 encoded_message += char  # Non-alphabetic characters remain unchanged
-
-        return(f"Used key of {rotation} to create:\n {encoded_message}")
-
+        leaving = (f"Original: {dinput}\nUsed key of {rotation} to create:\n{encoded_message}")
+        print(leaving)
+        basic_input=input(f"Please select an option:\n"
+                            f"[1]: Write/Overwrite file\n"
+                            f"[2]: Exit\n")
+        if int(basic_input) == 1:
+            newfile=input("Enter pre-existing file name or create a name")
+            f = open(newfile, "w")
+            f.write(leaving)
+            f.close()
+            return("Done added:\n"+ leaving)
+        else:
+            return("Sucessfully Exited")
 pass
 
 # encodes a target file, similarly to encode_message, except now targeting a filename
-def encode_file():
+def encode_file(file):
+    print(encode_message(file))
     pass
 
 # decodes target file using a user-specified key. If key is unknown, a keypress should
@@ -66,8 +77,7 @@ def main():
             try:
                 x = open(readW, "r")
                 f= (x.read())
-                print(encode_message(f))
-                encode_file()
+                encode_file(f)
             except FileNotFoundError:
                 print(f"Error: The file '{readW}' was not found.")
         elif selection == "3":
