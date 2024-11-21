92d00246f8c63ebb5a09f2b161d06eda3bba6ec0
pate9011 <pate9011@student.acitech.org>
2024-11-21 10:46:39 -0500

    11.21.24
    -Added the decode function to the program, and also fixed bugs with the encode program. Added decode function when nothing is inputted works great.
    -Have to add more comments, and then program officially finished.

--- enigma_template.py
+++ enigma_template.py
@@ -6,8 +6,21 @@
 # last update:  11.18.2024
 import random
 
-# we'll be using this string for the majority of our translations
-alphabet = "abcdefghijklmnopqrstuvwxyz"
+
+def file_save(leaving):
+    print(leaving)
+    basic_input = input(f"Please select an option:\n"
+                        f"[1]: Write/Overwrite file\n"
+                        f"[2]: Exit\n")
+    if int(basic_input) == 1:
+        newfile = input("Enter pre-existing file name or create a name")
+        f = open(newfile, "w")
+        f.write(leaving)
+        f.close()
+        return ("Done added:\n" + leaving)
+    else:
+        return ("Sucessfully Exited")
+pass
 
 # user inputs a message and selects a key (or random), the message is then translated using the cipher
 def encode_message(dinput):
@@ -19,7 +32,6 @@ def encode_message(dinput):
             rotation = int(basic_input) % 26  # Ensure rotation is within 0-25
 
         encoded_message = ""
-
         for char in dinput:
             if char.isalpha():  # Check if the character is a letter
                 # Shift character, rap around if necessary
@@ -28,33 +40,44 @@ def encode_message(dinput):
             else:
                 encoded_message += char  # Non-alphabetic characters remain unchanged
         leaving = (f"Original: {dinput}\nUsed key of {rotation} to create:\n{encoded_message}")
-        print(leaving)
-        basic_input=input(f"Please select an option:\n"
-                            f"[1]: Write/Overwrite file\n"
-                            f"[2]: Exit\n")
-        if int(basic_input) == 1:
-            newfile=input("Enter pre-existing file name or create a name")
-            f = open(newfile, "w")
-            f.write(leaving)
-            f.close()
-            return("Done added:\n"+ leaving)
-        else:
-            return("Sucessfully Exited")
+        print(file_save(leaving))
 pass
 
-# encodes a target file, similarly to encode_message, except now targeting a filename
-def encode_file(file):
-    print(encode_message(file))
-    pass
+def decode_file(dinput):
+    basic_input = input("Enter a rotation cipher key (Press Enter for a random value): ")
+    basic_input=0 if basic_input=="" else int(basic_input)
+    if basic_input==0:
+        decode_unknown_key(dinput)
+    else:
+        decoded_text = ""
+        for char in dinput:
+            if char.isalpha():
+                decoded_char = chr((ord(char) - ord('a') - int(basic_input)) % 26 + ord('a'))
+                decoded_text += decoded_char
+            else:
+                decoded_text += char
+        leaving = (f"Encoded: {dinput}\nUsed key of {basic_input} to create:\n{decoded_text}")
+        print(file_save(leaving))
+        return decoded_text
 
-# decodes target file using a user-specified key. If key is unknown, a keypress should
-# call decode_unknown_key()
-def decode_file():
-    pass
+pass
+
 
 # runs if the key is unknown. If this is true, print out all possible decoding combinations.
-def decode_unknown_key(filename):
-   pass
+def decode_unknown_key(dinput):
+    leaving=""
+    for x in range(26):
+        decoded_text = ""
+        for char in dinput:
+            if char.isalpha():
+                decoded_char = chr((ord(char) - ord('a') - int(x)) % 26 + ord('a'))
+                decoded_text += decoded_char
+            else:
+                decoded_text += char
+        leaving = leaving+(f"\nUsed key of {x} to create:\n{decoded_text}")
+    print(file_save(leaving))
+    return decoded_text
+pass
 
 
 # main method declaration
@@ -77,11 +100,17 @@ def main():
             try:
                 x = open(readW, "r")
                 f= (x.read())
-                encode_file(f)
+                print(encode_message(f))
             except FileNotFoundError:
                 print(f"Error: The file '{readW}' was not found.")
         elif selection == "3":
-            decode_file()
+            readW = input("What file would you like to be Decoded")
+            try:
+                x = open(readW, "r")
+                f= (x.read())
+                print(decode_file(f))
+            except FileNotFoundError:
+                print(f"Error: The file '{readW}' was not found.")
         elif selection == "4":
             print("Goodbye.")
             exit()
